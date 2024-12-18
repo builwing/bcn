6029acbb87444ad7669bc2843a215a4a30247e93
Hidemasa Nakada <nakada@winroad.jp>
2024-12-18 10:22:31 +0900

    tailwind.config.jsの修正 基本テーマの作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './components/**/*.{vue,js,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './composables/**/*.{js,ts}',
-    './plugins/**/*.{js,ts}',
-    './app.vue',
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./composables/**/*.{js,ts}",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f472b6', // ピンク系の色 (例)
+        secondary: '#f9a8d4', // セカンダリカラー (例)
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp')
-  ],
-}
-
+  plugins: [require("@tailwindcss/line-clamp")],
+};
