140affc9b015ed3903699e31bdf6bc3d06dd6149
Hidemasa Nakada <nakada@winroad.jp>
2024-12-23 17:26:03 +0900

    useApi.tsの作成

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -4,13 +4,16 @@ import type { NitroFetchRequest, NitroFetchOptions } from 'nitropack';
 export const useApi = () => {
     const config = useRuntimeConfig();
     const baseURL = config.public.apiBase;
+    const token = useCookie('token');
 
     // 共通のFetchオプション
     const defaultOptions: NitroFetchOptions<NitroFetchRequest> = {
         baseURL,
         credentials: 'include',
         headers: {
-            Accept: 'application/json',
+            'Accept': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+            ...(token.value ? { 'Authorization': `Bearer ${token.value}` } : {})
         },
     };
 
