53156b318606deab7f75f5f510462994a2f7ac6c
Hidemasa Nakada <nakada@winroad.jp>
2024-12-26 21:51:09 +0900

    些細な修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
     public: {
       apiBase: 'https://bcl.winroad.biz/api',
       sanctumEndpoint: 'https://bcl.winroad.biz/sanctum/csrf-cookie',
-      appVersion: '0.0.2'
+      appVersion: '0.0.3'
     },
   },
   build: {
